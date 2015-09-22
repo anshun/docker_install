b8180a641dc181e97f3cbf883adcc61c2d84b58a
An-Shun Liu <asliu823@gmail.com>
2015-09-22 13:40:17 +0800

    Modify setting for general use

--- install_docker.sh
+++ install_docker.sh
@@ -10,8 +10,12 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869
 sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
 sudo apt-get install lxc-docker -y
+sudo usermod -a -G docker $USER # add current user to docker group
+#sudo sed -i "s/#DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"/DOCKER_OPTS="-g /data/docker/temp"/g" /etc/default/docker
+#sudo service docker restart
 
 # Install docker-compose
+sudo apt-get install curl -y
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/1.4.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
@@ -20,5 +24,10 @@ sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/1.4.0/contr
 cd /tmp
 git clone https://gist.github.com/76b450a0c986e576e98b.git
 cd 76b450a0c986e576e98b
-sudo mv docker-cleanup /usr/local/bin/docker-cleanup
-sudo chmod +x /usr/local/bin/docker-cleanup
+sudo mv docker-cleanup /usr/local/bin/
+cd /usr/local/bin
+sudo chmod +x docker-cleanup
+sudo rm -rf /tmp/76b450a0c986e576e98b
+
+# Install make
+sudo apt-get install make -y
